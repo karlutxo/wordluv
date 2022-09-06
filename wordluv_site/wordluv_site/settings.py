e8a87faf9a4d5091ac798a91b77a422de2056250
Carlos on Mac <karlutxo@gmail.com>
2022-09-06 17:38:10 +0100

    secret key ammend

--- wordluv_site/wordluv_site/settings.py
+++ wordluv_site/wordluv_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os.path
 from pathlib import Path
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%v1o9_sgv8!v940qe0v#bjz=1aemtimx&vds44l-m!i8infnq$'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
