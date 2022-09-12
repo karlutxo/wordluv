4ea05e3a7cc8ff96749ba00d9d059942d4996994
Carlos on Linux <karlutxo@gmail.com>
2022-09-12 09:29:41 +0100

    kDict first tests

---
+++ wordluv_site/wordluv_app/kDict.py
@@ -0,0 +1,52 @@
+import requests
+import logging
+from decouple import config
+
+from bs4 import BeautifulSoup
+
+
+class kDict:
+    search_term = ""
+
+    def __init__(self, search_term):
+        self.search_term = search_term
+     
+    def lerobert(self):
+        meaning = []   
+        URL = config("URL_LRBT")+self.search_term
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        definition_section = soup.find("section", {"id": "definitions"})
+        # print(definition_section.prettify())
+        definitions = definition_section.find_all("div", class_="d_dvn")
+        for definition in definitions:
+        # print(definition.prettify())
+            d = definition.find("span", class_="d_dfn")
+            logging.debug(d)
+            if d is not None: 
+                meaning.append(d.text.strip())
+                #print(definition.find("span", class_="d_dfn").text.strip())
+                
+            d = definition.find("span", class_="d_xpl")
+            logging.debug(d)
+            if d is not None: 
+                meaning.append(d.text.strip())
+
+        return(meaning)
+
+    def wiktionary(self):
+        meaning = []   
+        URL = "https://fr.wiktionary.org/wiki/"+self.search_term
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        definition_div = soup.find("div", class_="mw-parser-output")
+        definition_ol  = definition_div.find("ol")
+        # print(definition_section.prettify())
+        definitions = definition_ol.find_all("li")
+        for d in definitions:
+        # print(d.prettify())
+            if d is not None: 
+                meaning.append(d.text.strip())
+                #print(definition.find("span", class_="d_dfn").text.strip())
+                
+        return(meaning)
---
+++ wordluv_site/wordluv_app/test_kDict.py
@@ -0,0 +1,27 @@
+import kDict
+import logging
+
+
+# falla con mechant
+
+# logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
+
+w = input("Introduce un termino: ")
+
+'''
+print(w + " según LeRobert....")
+d = kDict.kDict(w)
+ms = d.lerobert()
+n = 1
+for m in ms:
+    print(str(n) + ".- " +m)
+    n+=1
+    '''
+
+print(w + " según Wiktionary....")
+d = kDict.kDict(w)
+ms = d.wiktionary()
+n = 1
+for m in ms:
+    print(str(n) + ".- " +m)
+    n+=1    
