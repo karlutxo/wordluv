e360c82fdb69c75e8e365e43eeaff66c87c7a992
Carlos on Linux <karlutxo@gmail.com>
2022-09-13 13:20:51 +0100

    dict integrado

--- wordluv_site/wordluv_app/views.py
+++ wordluv_site/wordluv_app/views.py
@@ -1,4 +1,9 @@
 import genericpath
+from decouple import config
+import requests
+from bs4 import BeautifulSoup
+import logging
+
 
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -9,6 +14,8 @@ from django.views import generic
 
 from .models import word
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
 
 def WordListView(request):
     searchWord = request.GET.get('searchWord')
@@ -34,6 +41,34 @@ class WordDetailViewId(generic.DetailView):
 class WordDetailViewSlug(generic.DetailView):
     model = word
     template_name = 'wordDetail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        meaning = []   
+        URL = config("URL_LRBT") + self.kwargs.get('slug')
+        logging.debug(URL)
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        definition_section = soup.find("section", {"id": "definitions"})
+        definitions = definition_section.find_all("div", class_="d_ptma")
+#        definitions = definition_section.find_all("div", class_="d_dvn")
+#        logging.debug(definitions[0].prettify)
+        n = 0
+        for definition in definitions:
+            d = definition.find("span", class_="d_dfn")
+            if d is not None: 
+                n += 1
+                meaning.append(str(n) + ".- " + d.text.strip())
+#                logging.debug(d.text.strip())
+                                
+            d = definition.find("span", class_="d_xpl")
+            if d is not None: 
+                meaning.append("    ej.- " + d.text.strip())
+#                logging.debug(d.text.strip())
+
+        context['meaning'] = '\n'.join(meaning) 
+        logging.debug('\n'.join(meaning))
+        return context
 
 
 class WordUpdateView(UpdateView):
